60bbfc475cd0726e661f85f4d460227a96244ea7
Bartek <bartosz.walawender@gmail.com>
2021-12-04 13:25:32 +0100

    Day 4

--- advent_of_code_04_01.py
+++ advent_of_code_04_01.py
@@ -1,15 +1,46 @@
-values = []
-epsilon = gamma = ''
+class Bingo:
+    def __init__(self):
+        self.board = []
 
-with open('inputs/input_01_03.txt') as f:
-    values = f.readlines()
+    def insert_row(self, row):
+        self.board.append(row)
 
-for a in range(11):
-    sum = 0
-    for value in values:
-        sum = sum + int(value[a])
-    gamma = gamma + '1' if sum > len(values) / 2 else gamma + '0'
-    epsilon = epsilon + '0'if sum > len(values) / 2 else epsilon + '1'
+    def mark_number(self, called_val):
+        for row in self.board:
+            if called_val in row:
+                row[row.index(called_val)] = 0
 
-print(int(gamma, 2)*int(epsilon, 2))
-#508062
+    def check_bingo(self):
+        for i in range(5):
+            sum_col, sum_row = (0, 0)
+            for j in range(5):
+                sum_col += self.board[j][i]
+                sum_row += self.board[i][j]
+            if sum_col == 0 or sum_row == 0:
+                return True
+        return False
+
+    def get_sum(self):
+        board_sum = 0
+        for row in self.board:
+            board_sum += sum(row)
+        return board_sum
+
+
+bingo_boards = []
+with open('inputs/input_01_04.txt') as f:
+    values = [int(x) for x in f.readline().split(',')]
+    for i, line in enumerate(f):
+        if i % 6 == 0:
+            bingo_boards.append(Bingo())
+        else:
+            split = [int(x) for x in line.split()]
+            bingo_boards[-1].insert_row(split)
+
+for value in values:
+    for board in bingo_boards:
+        board.mark_number(value)
+        if board.check_bingo():
+            print(board.get_sum()*value)
+            # 12796
+            exit()
--- advent_of_code_04_02.py
+++ advent_of_code_04_02.py
@@ -5,31 +5,25 @@ class Bingo:
     def insert_row(self, row):
         self.board.append(row)
 
-    def print_board(self):
+    def mark_number(self, called_val):
         for row in self.board:
-            print(row)
-
-    def check_number(self, called_val):
-        for row in self.board:
-            for i, number in enumerate(row):
-                if number == called_val:
-                    row[i] = 0
+            if called_val in row:
+                row[row.index(called_val)] = 0
 
     def check_bingo(self):
         for i in range(5):
-            sum_col = 0
-            sum_row = 0
+            sum_col, sum_row = (0, 0)
             for j in range(5):
-                sum_col = sum_col + self.board[j][i]
-                sum_row = sum_row + self.board[i][j]
-            if 0 in (sum_col, sum_row):
+                sum_col += self.board[j][i]
+                sum_row += self.board[i][j]
+            if sum_col == 0 or sum_row == 0:
                 return True
         return False
 
     def get_sum(self):
         board_sum = 0
         for row in self.board:
-            board_sum = board_sum + sum(row)
+            board_sum += sum(row)
         return board_sum
 
 
@@ -40,17 +34,19 @@ with open('inputs/input_01_04.txt') as f:
         if i % 6 == 0:
             bingo_boards.append(Bingo())
         else:
-            stripped = [int(x) for x in line.split()]
-            bingo_boards[-1].insert_row(stripped)
+            split = [int(x) for x in line.split()]
+            bingo_boards[-1].insert_row(split)
 
 for value in values:
-    for board in bingo_boards:
-        a = 2
-        board.check_number(value)
+    for i in range(len(bingo_boards) - 1, -1, -1):
+        board = bingo_boards[i]
+        board.mark_number(value)
         if board.check_bingo():
-            print(board.get_sum()*value)
-            exit()
+            del bingo_boards[i]
+    if not bingo_boards:
+        break
+    last_to_win = bingo_boards[0]
 
-
-
+print(value*last_to_win.get_sum())
+# 18063
 
